2e3e675453ecabef8e8b155a330215827e9d20fd
Boi Schrader <boi1992@t-online.de>
2022-07-11 13:46:07 +0200

    5 confuses me

--- solution.js
+++ solution.js
@@ -1,18 +1,31 @@
 //Q1 Store Mark's and John's mass and height in variables.
-
+const markHeight = 1.65
+const markMass = 80
+const johnHeight = 1.77
+const johnMass = 95
 
 
 //Q2 Calculate both their BMIs and store their BMIs in variables. 
-
+const markBMI = (markMass / (markHeight * markHeight))
+const johnBMI = (johnMass / (johnHeight * johnHeight))
+console.log(markBMI , '&' , johnBMI)
 
 
 //Q3 Create a boolean variable containing information about whether Mark has a higher BMI than John.
-
+const markMoreBeefCake = markBMI >= johnBMI 
+console.log(markMoreBeefCake)
 
 
 //Q4 Print a string to the console containing the variable from step 3 using string interpolation. (e.g. "Is Mark's BMI higher than John's? Why yes, it's [boolean], it is"). 
+console.log("Q4: The statement that Mark has a higher BMI than John is" ,markMoreBeefCake)
+console.log("Q4: Is Mark's BMI higher than John's? Why yes, it's",markMoreBeefCake)
+
 
 
 
 //Q5 Create an *if* statement which prints the name and BMI of the person with the highest BMI. 
 
+
+if ((markBMI < johnBMI))
+    {console.log()}
+else{console.log(false)}
